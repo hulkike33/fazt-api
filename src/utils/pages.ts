6917651757daa7a041cf0d9ed5b437371297aefe
Federico Minaya <elfedeminaya@gmail.com>
2020-07-16 13:13:36 -0300

    fix(pagination): if limit is string show all items (#61)

--- src/utils/pages.ts
+++ src/utils/pages.ts
@@ -1,5 +1,6 @@
 export function getPages(pageNumber: string | number = 1, limit = 10) {
   const page = Number(pageNumber) - 1 || 0;
+  limit = isNaN(limit) ? 10 : limit;
   const skip = page * limit;
 
   return {
