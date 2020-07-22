296e5fb3da09e7e9bdb8fbca176906ccf919e6c0
PaoloTorregroza <paolodydtorregrosa@gmail.com>
2020-07-22 05:35:48 -0500

    refactor: new.routes.ts is now called news.routes.ts

--- src/routes/news.routes.ts
+++ src/routes/news.routes.ts
@@ -2,11 +2,15 @@
 import { Router } from 'express';
 import { handlerExceptionRoute } from '../error';
 import * as NewsCtrl from '../controllers/new.controller';
+import * as NewsValidator from '../validators/news.validator';
 const router = Router();
 
 router.get('/', handlerExceptionRoute(NewsCtrl.getNews));
 router.get('/:id', handlerExceptionRoute(NewsCtrl.getNew));
-router.post('/', handlerExceptionRoute(NewsCtrl.createNew));
+router.post(
+  '/', 
+  NewsValidator.createNewValidator,
+  handlerExceptionRoute(NewsCtrl.createNew));
 router.put('/:id', handlerExceptionRoute(NewsCtrl.updateNew));
 router.delete('/:id', handlerExceptionRoute(NewsCtrl.deleteNew));
 
