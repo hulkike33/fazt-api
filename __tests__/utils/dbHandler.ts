e825875c691c9b45bbb3865bbff28f1846f81dad
Fedeya <elfedeminaya@gmail.com>
2020-07-22 13:36:49 -0300

    fix(db): changed count for estimated document count

--- __tests__/utils/dbHandler.ts
+++ __tests__/utils/dbHandler.ts
@@ -24,7 +24,8 @@ export async function clearDatabase(): Promise<void> {
   const collections = mongoose.connection.collections;
   const collectionsDeleted = await Object.keys(collections)
     .map(async (collection) => {
-      if (await collections[collection].count()) return collections[collection].drop();
+      if (await collections[collection].estimatedDocumentCount())
+        return collections[collection].drop();
     })
     .filter((it) => it !== undefined);
 
