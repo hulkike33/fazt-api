755b4a7ad2ed345334333af8c4df72cae4fdc8ca
Fernando <lopezfer@globalhitss.com>
2020-08-04 20:32:52 -0300

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import * as swaggerDocument from './swagger.json';
+import * as swaggerUi from 'swagger-ui-express';
 
 import routes from './routes';
 
@@ -14,5 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
