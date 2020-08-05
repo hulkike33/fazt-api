c286f14d29db89bb9ef6358e32bc333c8641ef05
Cejas Claudio A <41306913+CejasClaudioA@users.noreply.github.com>
2020-08-05 20:07:50 -0300

    Feat: Entities added (#91)
    * feat(entities): add entities
    
    * fix(entities): remove schema and change uppercases
    
    * refactor(entities): change roles

---
+++ src/entities/rol-user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from './user.entity'
+import {Rol} from './rol.entity'
+
+@Index('uk_rol_user', ['id_rol,id_user'], { unique: true })
+@Entity("rol_user")
+export class RolUser {
+    @PrimaryGeneratedColumn({ type: "integer", name: "id" })
+    id!: number;
+
+    @Column("integer", { name: "state_rol" })
+    stateRol!: string;
+
+    @ManyToOne(() => Rol, rol => rol.id)
+    id_rol!: Rol
+
+    @ManyToOne(() => UserEntity, user => user.id)
+    id_user!: UserEntity
+}
---
+++ src/entities/rol.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { RolUser } from './rol-user.entity'
+
+@Index('uk_name_rol', ['nameRol'], { unique: true })
+@Entity("rol")
+export class Rol {
+    @PrimaryGeneratedColumn({ type: "integer", name: "id" })
+    id!: number;
+
+    @Column("integer", { name: "name_rol" })
+    nameRol!: string;
+
+    @OneToMany(() => RolUser, rolUser => rolUser.id)
+    rolUser!: RolUser[];
+}
