54ef6a05aad49cbfccdf57c23a09918b7b70badd
Federico Minaya <elfedeminaya@gmail.com>
2020-08-05 20:48:28 -0300

    fix(unique): fixed unique in rol_user entity (#92)
    * fix(unique): fixed unique in rol_user entity
    
    * refactor: removed !

--- src/entities/rol-user.entity.ts
+++ src/entities/rol-user.entity.ts
@@ -1,19 +1,19 @@
 import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { UserEntity } from './user.entity'
-import {Rol} from './rol.entity'
+import { UserEntity } from './user.entity';
+import { Rol } from './rol.entity';
 
-@Index('uk_rol_user', ['id_rol,id_user'], { unique: true })
-@Entity("rol_user")
+@Index('uk_rol_user', ['id_rol', 'id_user'], { unique: true })
+@Entity('rol_user')
 export class RolUser {
-    @PrimaryGeneratedColumn({ type: "integer", name: "id" })
-    id!: number;
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  id: number;
 
-    @Column("integer", { name: "state_rol" })
-    stateRol!: string;
+  @Column('integer', { name: 'state_rol' })
+  stateRol: string;
 
-    @ManyToOne(() => Rol, rol => rol.id)
-    id_rol!: Rol
+  @ManyToOne(() => Rol, rol => rol.id)
+  id_rol: Rol;
 
-    @ManyToOne(() => UserEntity, user => user.id)
-    id_user!: UserEntity
+  @ManyToOne(() => UserEntity, user => user.id)
+  id_user: UserEntity;
 }
--- src/entities/rol.entity.ts
+++ src/entities/rol.entity.ts
@@ -1,15 +1,15 @@
 import { Column, Entity, Index, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
-import { RolUser } from './rol-user.entity'
+import { RolUser } from './rol-user.entity';
 
 @Index('uk_name_rol', ['nameRol'], { unique: true })
-@Entity("rol")
+@Entity('rol')
 export class Rol {
-    @PrimaryGeneratedColumn({ type: "integer", name: "id" })
-    id!: number;
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  id: number;
 
-    @Column("integer", { name: "name_rol" })
-    nameRol!: string;
+  @Column('integer', { name: 'name_rol' })
+  nameRol: string;
 
-    @OneToMany(() => RolUser, rolUser => rolUser.id)
-    rolUser!: RolUser[];
+  @OneToMany(() => RolUser, rolUser => rolUser.id)
+  rolUser: RolUser[];
 }
